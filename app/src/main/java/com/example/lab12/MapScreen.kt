9d5361240388080c8736d1ad90e375db345a47cd
JhosephvA <henry.ventura@tecsup.edu.pe>
2025-06-11 21:50:58 -0500

    Dibujar formas en el mapa

--- app/src/main/java/com/example/lab12/MapScreen.kt
+++ app/src/main/java/com/example/lab12/MapScreen.kt
@@ -5,8 +5,11 @@ import android.graphics.BitmapFactory
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.*
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.model.BitmapDescriptor
@@ -14,6 +17,8 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.Polygon
+import com.google.maps.android.compose.Polyline
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
 import kotlinx.coroutines.Dispatchers
@@ -26,7 +31,7 @@ fun MapScreen() {
     val cameraPositionState = rememberCameraPositionState {
         position = com.google.android.gms.maps.model.CameraPosition.fromLatLngZoom(arequipaLocation, 12f)
     }
-    
+
     LaunchedEffect(Unit) {
         cameraPositionState.animate(
             update = CameraUpdateFactory.newLatLngZoom(LatLng(-16.2520984, -71.6836503), 12f), // Yura
@@ -73,6 +78,60 @@ fun MapScreen() {
                         snippet = "Punto de interés"
                     )
                 }
+
+                // 🔷 Polígonos
+                val mallAventuraPolygon = listOf(
+                    LatLng(-16.432292, -71.509145),
+                    LatLng(-16.432757, -71.509626),
+                    LatLng(-16.433013, -71.509310),
+                    LatLng(-16.432566, -71.508853)
+                )
+
+                val parqueLambramaniPolygon = listOf(
+                    LatLng(-16.422704, -71.530830),
+                    LatLng(-16.422920, -71.531340),
+                    LatLng(-16.423264, -71.531110),
+                    LatLng(-16.423050, -71.530600)
+                )
+
+                val plazaDeArmasPolygon = listOf(
+                    LatLng(-16.398866, -71.536961),
+                    LatLng(-16.398744, -71.536529),
+                    LatLng(-16.399178, -71.536289),
+                    LatLng(-16.399299, -71.536721)
+                )
+
+                Polygon(
+                    points = plazaDeArmasPolygon,
+                    strokeColor = Color.Red,
+                    fillColor = Color.Blue.copy(alpha = 0.3f),
+                    strokeWidth = 5f
+                )
+                Polygon(
+                    points = parqueLambramaniPolygon,
+                    strokeColor = Color.Red,
+                    fillColor = Color.Green.copy(alpha = 0.3f),
+                    strokeWidth = 5f
+                )
+                Polygon(
+                    points = mallAventuraPolygon,
+                    strokeColor = Color.Red,
+                    fillColor = Color.Yellow.copy(alpha = 0.3f),
+                    strokeWidth = 5f
+                )
+                
+                val rutaCompleta = listOf(
+                    LatLng(-16.398866, -71.536961),  // Plaza de Armas
+                    LatLng(-16.4205151, -71.4945209),  // Paucarpata
+                    LatLng(-16.3524187, -71.5675994),  // Zamacola
+                    LatLng(-16.392010, -71.548901)   // Yanahuara (reemplaza a Yura)
+                )
+
+                Polyline(
+                    points = rutaCompleta,
+                    color = Color.Magenta,
+                    width = 6f
+                )
             }
         }
     }
